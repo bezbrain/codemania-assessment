3375349e6d4410a249fb2580cf31e69c1a1a344b
bezbrain <belloquadriolawale@gmail.com>
2024-09-28 16:46:38 +0100

    Develop the fixture component

--- src/components/routes/tournamentsComp/others/tournamentFixtures.tsx
+++ src/components/routes/tournamentsComp/others/tournamentFixtures.tsx
@@ -1,105 +1,134 @@
-import {
-  SingleEliminationBracket,
-  Match,
-  SVGViewer,
-} from "@g-loot/react-tournament-brackets";
-
-// Define the static matches data
-const matches = [
-  {
-    id: 1,
-    name: "Quarterfinal 1",
-    nextMatchId: 5, // Points to the semifinal
-    tournamentRoundText: "Quarterfinal",
-    state: "DONE",
-    participants: [
-      { id: "team1", name: "Team A", isWinner: true, resultText: "WON" },
-      { id: "team2", name: "Team B", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 2,
-    name: "Quarterfinal 2",
-    nextMatchId: 5,
-    tournamentRoundText: "Quarterfinal",
-    state: "DONE",
-    participants: [
-      { id: "team3", name: "Team C", isWinner: true, resultText: "WON" },
-      { id: "team4", name: "Team D", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 3,
-    name: "Quarterfinal 3",
-    nextMatchId: 6,
-    tournamentRoundText: "Quarterfinal",
-    state: "DONE",
-    participants: [
-      { id: "team5", name: "Team E", isWinner: true, resultText: "WON" },
-      { id: "team6", name: "Team F", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 4,
-    name: "Quarterfinal 4",
-    nextMatchId: 6,
-    tournamentRoundText: "Quarterfinal",
-    state: "DONE",
-    participants: [
-      { id: "team7", name: "Team G", isWinner: true, resultText: "WON" },
-      { id: "team8", name: "Team H", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 5,
-    name: "Semifinal 1",
-    nextMatchId: 7,
-    tournamentRoundText: "Semifinal",
-    state: "DONE",
-    participants: [
-      { id: "team1", name: "Team A", isWinner: true, resultText: "WON" },
-      { id: "team3", name: "Team C", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 6,
-    name: "Semifinal 2",
-    nextMatchId: 7,
-    tournamentRoundText: "Semifinal",
-    state: "DONE",
-    participants: [
-      { id: "team5", name: "Team E", isWinner: true, resultText: "WON" },
-      { id: "team7", name: "Team G", isWinner: false, resultText: "LOST" },
-    ],
-  },
-  {
-    id: 7,
-    name: "Final",
-    nextMatchId: null,
-    tournamentRoundText: "Final",
-    state: "DONE",
-    participants: [
-      { id: "team1", name: "Team A", isWinner: true, resultText: "WON" },
-      { id: "team5", name: "Team E", isWinner: false, resultText: "LOST" },
-    ],
-  },
-];
-
-// The component rendering the static bracket
 const TournamentFixtures = () => {
-  const finalWidth = 800;
-  const finalHeight = 600;
-
   return (
-    <SingleEliminationBracket
-      matches={matches}
-      matchComponent={Match}
-      svgWrapper={({ children, ...props }: any) => (
-        <SVGViewer width={finalWidth} height={finalHeight} {...props}>
-          {children}
-        </SVGViewer>
-      )}
-    />
+    <section className="overflow-x-auto">
+      <section className="relative w-[900px] largeTab:w-full">
+        {/* LHS */}
+        <div className="space-y-12 relative">
+          {/* Left First set */}
+          <div>
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+
+            <div className="relative h-12 w-[100px]">
+              <hr className="absolute w-3 -top-[20px] -right-3" />
+              <hr className="absolute top-6 w-[5.5rem] -right-14 rotate-90" />
+              <hr className="absolute w-3 -bottom-[20px] -right-3" />
+              <hr className="absolute w-3 top-1/2 -right-6" />
+            </div>
+
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+          </div>
+
+          {/* Left Second set */}
+          <div className="">
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+
+            <div className="relative h-12 w-[100px]">
+              <hr className="absolute w-3 -top-[20px] -right-3" />
+              <hr className="absolute top-6 w-[5.5rem] -right-14 rotate-90" />
+              <hr className="absolute w-3 -bottom-[20px] -right-3" />
+              <hr className="absolute w-3 top-1/2 -right-6" />
+            </div>
+
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+          </div>
+
+          {/* Left Final Set */}
+          <div className="absolute -top-2 left-[7.5rem] h-[190px] flex flex-col justify-between">
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+
+            <div className="relative h-32 w-[100px]">
+              <hr className="absolute w-3 -top-[20px] -right-3" />
+              <hr className="absolute top-[62px] w-[165px] -right-[94px] rotate-90" />
+              <hr className="absolute w-3 -bottom-[20px] -right-3" />
+              <hr className="absolute w-3 top-1/2 -right-6" />
+            </div>
+
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+              <span>TBD</span>
+            </div>
+          </div>
+
+          {/* The Left winner */}
+          <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1 absolute top-[32%] -translate-y-1/2 left-60">
+            <span>TBD</span>
+          </div>
+        </div>
+
+        {/* RHS */}
+        <div className="absolute top-0 right-4">
+          <div className="space-y-12">
+            {/*Right First set */}
+            <div>
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+
+              <div className="relative h-12 w-[100px]">
+                <hr className="absolute w-3 -top-[20px] right-[6.2rem]" />
+                <hr className="absolute top-6 w-[5.5rem] right-[4.2rem] rotate-90" />
+                <hr className="absolute w-3 -bottom-[20px] right-[6.2rem]" />
+                <hr className="absolute w-3 top-1/2 right-[7rem]" />
+              </div>
+
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+            </div>
+
+            {/* Right Second set */}
+            <div>
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+
+              <div className="relative h-12 w-[100px]">
+                <hr className="absolute w-3 -top-[20px] right-[6.2rem]" />
+                <hr className="absolute top-6 w-[5.5rem] right-[4.2rem] rotate-90" />
+                <hr className="absolute w-3 -bottom-[20px] right-[6.2rem]" />
+                <hr className="absolute w-3 top-1/2 right-[7rem]" />
+              </div>
+
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+            </div>
+
+            {/* Right Final Set */}
+            <div className="absolute -top-2 right-[7.5rem] h-[190px] flex flex-col justify-between">
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+
+              <div className="relative h-32 w-[100px]">
+                <hr className="absolute w-3 -top-[20px] right-[6.2rem]" />
+                <hr className="absolute top-[62px] w-[165px] right-[28px] rotate-90" />
+                <hr className="absolute w-3 -bottom-[20px] right-[6.2rem]" />
+                <hr className="absolute w-3 top-1/2 right-[6.9rem]" />
+              </div>
+
+              <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1">
+                <span>TBD</span>
+              </div>
+            </div>
+
+            {/* The Right winner */}
+            <div className="bg-[#3b3b3b] w-[100px] text-center text-white py-1 absolute top-[32%] -translate-y-1/2 right-60">
+              <span>TBD</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
   );
 };
 
